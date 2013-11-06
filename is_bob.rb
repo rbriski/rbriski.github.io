46b96fc8a58ea092c1d7c02d5c1117cc7fe915b5
Bob Briski <rbriski@SEMDevel>
2013-11-06 09:40:50 -0800

    add yaml

--- is_bob.rb
+++ is_bob.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
+require 'yaml'
 
 class IsBob < Sinatra::Base
   get '/' do
     redis = Redis.new
     s = redis.get "hubot:storage"
-    y = YAML.load(s)
+    y = ::YAML.load(s)
     if y['locations']['Bob Briski'] == 'mtv'
       return 'Yes'
     else
