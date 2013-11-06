6bd01eca966fb14d7bc2be94d8319966cc2c9e91
Bob Briski <rbriski@SEMDevel>
2013-11-06 09:37:03 -0800

    more automation

---
+++ config.ru
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require './is_bob'
+run IsBob
---
+++ is_bob.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+
+class IsBob < Sinatra::Base
+  get '/' do
+    redis = Redis.new
+    s = redis.get "hubot:storage"
+    y = YAML.load(s)
+    if y['locations']['Bob Briski'] == 'mtv'
+      return 'Yes'
+    else
+      return "Nope. In #{y['locations']['Bob Briski']}"
+    end
+  end
+end
